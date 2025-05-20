98693146b14eedf84481de13295453f41284ac4c
id7xyz <id7xyz@gmail.com>
2025-05-20 21:36:45 +0300

    hopfully GTK themes work

---
+++ personal_scripts/.local/bin/personal/discord.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# _     _ _____
+#(_) __| |___  |_  ___   _ ____
+#| |/ _| |  / /\ \/ / | | |_  /
+#| | (_| | / /  >  <| |_| |/ /
+#|_|\__,_|/_/  /_/\_\__, /___|
+# Created by: d7eeem aka id7xyz
+# https://gitlab.com/d7eeem
+
+
+if command -v discord &> /dev/null; then
+  discord &
+else
+  flatpak run com.discordapp.Discord &
+fi
