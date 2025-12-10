1dd95018f9c065e762dcffa7461293db45dd240e
d7eeem <id7xyz@gmail.com>
2025-12-10 08:53:24 +0300

    using zanity insted of yad

--- askpass-helper.sh
+++ askpass-helper.sh
@@ -1,4 +1,8 @@
 #!/bin/env bash
 set -e
 
-yad --entry --hide-text --title="Authentication Required" --text="Enter your password:"
+#yad --entry --hide-text --title="Authentication Required" --text="Enter your password:"
+
+zenity --password \
+       --title="Authentication Required" \
+       --text="Enter your password:"
